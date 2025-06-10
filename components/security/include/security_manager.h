72477cfe1e2bde3cb8be4b9f418971135d27c616
Frank Amoah <121383711+amoahfrank@users.noreply.github.com>
2025-06-10 23:44:10 +0100

    Update security_manager.h
    Authorship updated.

--- components/security/include/security_manager.h
+++ components/security/include/security_manager.h
@@ -5,7 +5,7 @@
  * This file defines the public interface for the security manager,
  * which handles encryption, secure storage, and TLS/SSL configuration.
  * 
- * @author Smart Meter Reader OCR Team
+ * @author Frank Amoah A.K.A SiRWaTT Smart Meter Reader OCR Team Lead
  * @date 2023
  */
 
