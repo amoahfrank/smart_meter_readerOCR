8c35aa2be01c262f319bf3f3de836788f9d99356
Frank Amoah <121383711+amoahfrank@users.noreply.github.com>
2025-06-10 23:32:14 +0100

    Update image_processing.h
    Authorship update.

--- components/ocr/include/image_processing.h
+++ components/ocr/include/image_processing.h
@@ -5,7 +5,7 @@
  * This file defines the public interface for the image processing module,
  * which prepares images for OCR recognition.
  * 
- * @author Smart Meter Reader OCR Team
+ * @author Frank Amoah A.K.A SiRWaTT Smart Meter Reader OCR Team Lead
  * @date 2023
  */
 
