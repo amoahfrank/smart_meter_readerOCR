f84113fb5fa49edeabb186890bdca050d8c14dbc
Frank Amoah <121383711+amoahfrank@users.noreply.github.com>
2025-06-10 23:18:15 +0100

    Update wifi_manager.h
    Authorship updated.

--- components/connectivity/include/wifi_manager.h
+++ components/connectivity/include/wifi_manager.h
@@ -5,7 +5,7 @@
  * This file defines the public interface for the WiFi connectivity manager,
  * which handles WiFi connection and data transmission.
  * 
- * @author Smart Meter Reader OCR Team
+ * @author Frank Amoah A.K.A SiRWaTT Smart Meter Reader OCR Team Lead
  * @date 2023
  */
 
