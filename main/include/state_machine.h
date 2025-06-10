bbb65ceda27e35fe712550a08486e59d70303b47
Frank Amoah <121383711+amoahfrank@users.noreply.github.com>
2025-06-10 23:05:41 +0100

    Update state_machine.h
    Authorship updated.

--- main/include/state_machine.h
+++ main/include/state_machine.h
@@ -5,7 +5,7 @@
  * This file defines the public interface for the state machine,
  * which controls the operational flow of the device.
  * 
- * @author Smart Meter Reader OCR Team
+ * @author Frank Amoah A.K.A SiRWaTT Smart Meter Reader OCR Team Lead
  * @date 2023
  */
 
